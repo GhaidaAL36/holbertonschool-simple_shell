19ed4685c42e8c68fe8a2587c302ea3c8cde82a5
lina_aziz <lenaexo0@gmail.com>
2025-11-06 01:09:08 +0300

    Update simple_shell.h

--- simple_shell.h
+++ simple_shell.h
@@ -15,9 +15,5 @@ char *read_line(void);
 char **split_line(char *line);
 void execute_command(char **args);
 void print_prompt(void);
-int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
-char *_strcat(char *dest, char *src);
-int _strcmp(char *s1, char *s2);
 
 #endif
