c8e5aa6571c2cd7c5b64c033a37f5fffe87f6432
lina_aziz <lenaexo0@gmail.com>
2025-11-05 14:52:10 +0300

    Update execute_command.c

--- execute_command.c
+++ execute_command.c
@@ -1,23 +1,22 @@
 #include "simple_shell.h"
 
 /**
- * execute_command - Execute a command with PATH support
- * @command: user input
- * @program_name: name of shell program for error prints
+ * execute_command - Execute a command from input
+ * @command: input string
+ * @program_name: program name for error printing
  */
 void execute_command(char *command, char *program_name)
 {
-	char *args[100];
-	char *cmd_copy = strdup(command);
+	char *args[64];
 	char *token;
 	int i = 0;
 	char *path, *path_copy, *dir;
 	char full_path[1024];
-	pid_t pid;
 	int found = 0;
+	pid_t pid;
 
-	/* split input to args[] */
-	token = strtok(cmd_copy, " ");
+	/* tokenize input -> args[] */
+	token = strtok(command, " ");
 	while (token != NULL)
 	{
 		args[i++] = token;
@@ -25,7 +24,7 @@ void execute_command(char *command, char *program_name)
 	}
 	args[i] = NULL;
 
-	/* case: absolute/relative path command */
+	/* If there's '/' -> execute directly */
 	if (strchr(args[0], '/'))
 	{
 		if (access(args[0], X_OK) == 0)
@@ -35,29 +34,22 @@ void execute_command(char *command, char *program_name)
 			{
 				execve(args[0], args, environ);
 				perror(program_name);
-				free(cmd_copy);
 				exit(1);
 			}
 			wait(NULL);
-			free(cmd_copy);
 			return;
 		}
 		dprintf(STDERR_FILENO, "%s: %s: not found\n", program_name, args[0]);
-		free(cmd_copy);
 		return;
 	}
 
-	/* search in PATH */
+	/* PATH search */
 	path = getenv("PATH");
 	if (!path)
-	{
-		free(cmd_copy);
 		return;
-	}
-
 	path_copy = strdup(path);
-	dir = strtok(path_copy, ":");
 
+	dir = strtok(path_copy, ":");
 	while (dir != NULL)
 	{
 		snprintf(full_path, sizeof(full_path), "%s/%s", dir, args[0]);
@@ -68,24 +60,22 @@ void execute_command(char *command, char *program_name)
 		}
 		dir = strtok(NULL, ":");
 	}
+
 	free(path_copy);
 
 	if (!found)
 	{
 		dprintf(STDERR_FILENO, "%s: %s: not found\n", program_name, args[0]);
-		free(cmd_copy);
 		return;
 	}
 
+	/* execute */
 	pid = fork();
 	if (pid == 0)
 	{
 		execve(full_path, args, environ);
 		perror(program_name);
-		free(cmd_copy);
 		exit(1);
 	}
-
 	wait(NULL);
-	free(cmd_copy);
 }
