447c47cdedd69ea31bf1af283d61327d06dd4ca4
lina_aziz <lenaexo0@gmail.com>
2025-11-06 01:58:18 +0300

    Update handlePath.c

--- handlePath.c
+++ handlePath.c
@@ -1,38 +1,38 @@
 #include "main.h"
 
-char *handle_path(char *cmd)
+char *handle_path(const char *cmd)
 {
-	char *path = getenv("PATH");
-	char *copy, *token, *full;
-	struct stat st;
+    char *path = getenv("PATH");
+    char *copy, *token, *full;
+    struct stat st;
 
-	if (!cmd)
-		return (NULL);
+    if (!cmd || !path)
+        return NULL;
 
-	if (cmd[0] == '/' || cmd[0] == '.')
-	{
-		if (stat(cmd, &st) == 0)
-			return (cmd);
-		return (NULL);
-	}
+    if (cmd[0] == '/' || cmd[0] == '.')
+    {
+        if (stat(cmd, &st) == 0)
+            return strdup(cmd);
+        return NULL;
+    }
 
-	copy = strdup(path);
-	token = strtok(copy, ":");
-	while (token)
-	{
-		full = malloc(strlen(token) + strlen(cmd) + 2);
-		strcpy(full, token);
-		strcat(full, "/");
-		strcat(full, cmd);
+    copy = strdup(path);
+    token = strtok(copy, ":");
+    while (token)
+    {
+        full = malloc(strlen(token) + strlen(cmd) + 2);
+        strcpy(full, token);
+        strcat(full, "/");
+        strcat(full, cmd);
 
-		if (stat(full, &st) == 0)
-		{
-			free(copy);
-			return (full);
-		}
-		free(full);
-		token = strtok(NULL, ":");
-	}
-	free(copy);
-	return (NULL);
+        if (stat(full, &st) == 0)
+        {
+            free(copy);
+            return full;
+        }
+        free(full);
+        token = strtok(NULL, ":");
+    }
+    free(copy);
+    return NULL;
 }
