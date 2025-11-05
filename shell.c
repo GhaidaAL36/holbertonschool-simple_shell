7ab97baa02253830ea44992ac296f4331a1bcb9a
lina_aziz <lenaexo0@gmail.com>
2025-11-06 01:57:42 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,65 +1,82 @@
 #include "main.h"
 
+int is_interactive(void)
+{
+    return isatty(STDIN_FILENO);
+}
+
 char *read_line(void)
 {
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t nread;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t n = getline(&line, &len, stdin);
 
-	nread = getline(&line, &len, stdin);
-	if (nread == -1)
-	{
-		free(line);
-		exit(0);
-	}
-	return (line);
+    if (n == -1)
+    {
+        free(line);
+        return NULL;
+    }
+    return line;
 }
 
 char **tokenize(char *line)
 {
-	char **tokens, *token;
-	int i = 0;
-	size_t size = 64;
+    char **args = NULL, *tok;
+    size_t cap = 8, i = 0;
 
-	tokens = malloc(size * sizeof(char *));
-	if (!tokens)
-		exit(1);
+    args = malloc(sizeof(char *) * cap);
+    if (!args)
+        return NULL;
 
-	token = strtok(line, " \t\r\n");
-	while (token)
-	{
-		tokens[i++] = token;
-		token = strtok(NULL, " \t\r\n");
-	}
-	tokens[i] = NULL;
-	return (tokens);
+    tok = strtok(line, " \t\r\n");
+    while (tok)
+    {
+        if (i + 1 >= cap)
+        {
+            cap <<= 1;
+            args = realloc(args, sizeof(char *) * cap);
+            if (!args)
+                return NULL;
+        }
+        args[i++] = tok;
+        tok = strtok(NULL, " \t\r\n");
+    }
+    args[i] = NULL;
+    return args;
 }
 
 int main(void)
 {
-	char *line, **args;
+    char *line, **argv;
+    int inter = is_interactive();
 
-	while (1)
-	{
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "$ ", 2);
-		line = read_line();
-		args = tokenize(line);
-		if (!args[0])
-		{
-			free(line);
-			free(args);
-			continue;
-		}
-		if (strcmp(args[0], "exit") == 0)
-		{
-			free(line);
-			free(args);
-			break;
-		}
-		execute(args);
-		free(line);
-		free(args);
-	}
-	return (0);
+    while (1)
+    {
+        if (inter)
+            write(STDOUT_FILENO, "$ ", 2);
+
+        line = read_line();
+        if (!line)
+            break;
+
+        argv = tokenize(line);
+        if (!argv || !argv[0])
+        {
+            free(argv);
+            free(line);
+            continue;
+        }
+
+        if (strcmp(argv[0], "exit") == 0)
+        {
+            free(argv);
+            free(line);
+            break;
+        }
+
+        execute(argv);
+        free(argv);
+        free(line);
+    }
+    return 0;
 }
