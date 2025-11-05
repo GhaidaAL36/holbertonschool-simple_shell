531da2db26044f5dd4a8a72fde4ec41a106a53b3
lina_aziz <lenaexo0@gmail.com>
2025-11-06 00:51:06 +0300

    Update simple_shell.h

--- simple_shell.h
+++ simple_shell.h
@@ -1,18 +1,15 @@
-#ifndef SIMPLE_SHELL_H
-#define SIMPLE_SHELL_H
+#ifndef SHELL_H
+#define SHELL_H
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
 
-void print_prompt(void);
-ssize_t read_input(char **input, size_t *len);        /* getline-style */
-char *trim_whitespace(char *str);
-void execute_command(char *command, char *program_name);
+char **parse_line(char *line);
+void execute_command(char **args, char **env);
 
-extern char **environ;
-
-#endif /* SIMPLE_SHELL_H */
+#endif
