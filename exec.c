c8222e20d8ac1653cd4bde2b213afb485a66e6a0
lina_aziz <lenaexo0@gmail.com>
2025-11-06 01:46:50 +0300

    Update exec.c

--- exec.c
+++ exec.c
@@ -3,7 +3,7 @@
 void execute(char **argv)
 {
 	pid_t pid;
-	char *path;
+	char *cmd_path;
 
 	if (!argv || !argv[0])
 		return;
@@ -14,29 +14,23 @@ void execute(char **argv)
 		return;
 	}
 
-	path = argv[0];
-	if (path[0] != '/' && path[0] != '.')
+	cmd_path = handle_path(argv[0]);
+	if (!cmd_path)
 	{
-		char *resolved = find_in_path(argv[0]);
-		if (resolved)
-			path = resolved;
-		else
-		{
-			write(STDERR_FILENO, "not found\n", 10);
-			return;
-		}
+		write(STDERR_FILENO, "not found\n", 10);
+		return;
 	}
 
 	pid = fork();
 	if (pid == 0)
 	{
-		if (execve(path, argv, environ) == -1)
+		if (execve(cmd_path, argv, environ) == -1)
 			perror("execve");
 		exit(127);
 	}
 	else if (pid > 0)
 		waitpid(pid, NULL, 0);
 
-	if (path != argv[0])
-		free(path);
+	if (strcmp(cmd_path, argv[0]) != 0)
+		free(cmd_path);
 }
