836d9ebe9ce1bed86fd91a77d74d07cc8905f424
lina_aziz <lenaexo0@gmail.com>
2025-11-06 02:51:45 +0300

    Create utils.c

---
+++ utils.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+int _strcmp(char *s1, char *s2)
+{
+    int i = 0;
+
+    while (s1[i] && s2[i])
+    {
+        if (s1[i] != s2[i])
+            return (s1[i] - s2[i]);
+        i++;
+    }
+    return (s1[i] - s2[i]);
+}
