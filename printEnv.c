70656d331b837adcccaf9ba74ee358e9baa2c05c
lina_aziz <lenaexo0@gmail.com>
2025-11-06 01:58:34 +0300

    Update printEnv.c

--- printEnv.c
+++ printEnv.c
@@ -2,12 +2,12 @@
 
 void print_env(void)
 {
-	int i = 0;
+    int i = 0;
 
-	while (environ[i])
-	{
-		write(STDOUT_FILENO, environ[i], strlen(environ[i]));
-		write(STDOUT_FILENO, "\n", 1);
-		i++;
-	}
+    while (environ[i])
+    {
+        write(STDOUT_FILENO, environ[i], strlen(environ[i]));
+        write(STDOUT_FILENO, "\n", 1);
+        i++;
+    }
 }
