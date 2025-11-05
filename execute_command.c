1633cd3b24cf18edfe71daae79be04f45034f52f
lina_aziz <lenaexo0@gmail.com>
2025-11-05 10:00:57 +0300

    Update execute_command.c

--- execute_command.c
+++ execute_command.c
@@ -1,125 +1,126 @@
-#include "simpleshell.h"
-
-/*char *fullpath;*/
-
-/**
- * printerror - prints error
- * @command: command inserted
- */
-
-void printerror(char *const command[])
-{
-	fprintf(stderr, "./hsh: 1: %s: not found\n", command[0]);
-	free(*command);
-	exit(127);
-}
+#include "simple_shell.h"
+#include <unistd.h>
+extern char **environ;
 
 /**
- * _getenv - get environment variable
- * @name: name of environment variable
- * @envp: pointer to environment variables
- * Return: pointer to environment variable
+ * execute_command - Executes a command entered by the user.
+ * @command: The command to execute (e.g., "/bin/ls").
+ * @program_name: The name of the shell program (for error messages).
+ * execute_command - Execute a command entered by the user
+ * @command: The full input string from user
+ * @args: Parsed command and arguments (args[0] = command)
+ * @program_name: Name of the shell program
+ *
+ * Return: Nothing
  */
-
-char *_getenv(const char *name, char **envp)
+void execute_command(char *command, char *program_name)
+void execute_command(char *command, char **args, char *program_name)
 {
-	char **env, *separator;
+	char *args[200];
+	pid_t pid;
+	char *token;
+	int i = 0;
 
-	for (env = envp; *env != NULL; env++)
+	char *path;
+	char *path_copy;
+	char *dir;
+	char *path, *path_copy, *dir;
+	char full_path[1024];
+	int found = 0;
+	pid_t pid;
+
+	token = strtok(command, " ");
+	while (token != NULL)
+	/* Built-in: exit */
+	if (strcmp(args[0], "exit") == 0)
 	{
-		separator = strchr(*env, '=');
-		if (separator != NULL)
+		args[i++] = token;
+		token = strtok(NULL, " ");
+		free(command);
+		exit(0);
+	}
+
+	args[i] = NULL;
+	if (access(args[0], X_OK) != 0)
+	/* If command contains '/', try executing directly */
+	if (strchr(args[0], '/') != NULL)
+	{
+		path = getenv("PATH");
+		path_copy = strdup(path);
+		dir = strtok(path_copy, ":");
+
+		while (dir != NULL)
+		if (access(args[0], X_OK) == 0)
 		{
-			if (strncmp(*env, name, separator - *env) == 0)
+			snprintf(full_path, sizeof(full_path), "%s/%s", dir, args[0]);
+			if (access(full_path, X_OK) == 0)
+			pid = fork();
+			if (pid == 0)
 			{
-				return (separator + 1);
+				args[0] = strdup(full_path);
+				found = 1;
+				break;
+				execve(args[0], args, environ);
+				perror(program_name);
+				exit(1);
 			}
+			dir = strtok(NULL, ":");
+			wait(NULL);
+			return;
 		}
-	}
-	return (NULL);
-}
-
-/**
- * pathfinder - finds the correct path for given alias
- * @command: given input to check for path
- * @cmd: argument
- * @envp: pointer to environment variables
- * Return: pointer to the string array or NULL if failed
- */
-
-char **pathfinder(char *cmd, char **command, char **envp)
+		if (!found)
 {
-	char *current_path, *temp_path;
-	char *path_tok;
-	size_t arglen = strlen(cmd);
-
-	fullpath = NULL;
-
-	if (strchr(cmd, '/') != NULL && access(cmd, F_OK) == 0)
-	{
-		command[0] = cmd;
-		return (command);
-	}
-
-	path_tok = NULL;
-	current_path = _getenv("PATH", envp);
-	temp_path = strdup(current_path);
-	path_tok = strtok(temp_path, ":");
-
-	while (path_tok)
-	{
-		fullpath = malloc(arglen + strlen(path_tok) + 2);
-		sprintf(fullpath, "%s/%s", path_tok, cmd);
-		if (access(fullpath, F_OK) == 0)
-		{
-			command[0] = fullpath;
-			free(temp_path);
-			return (command);
-		}
-		path_tok = strtok(NULL, ":");
-		free(fullpath);
-	}
-	free(temp_path);
-	return (NULL);
+    dprintf(STDERR_FILENO, "hsh: %s: not found\n", args[0]);
+    exit(127);
 }
-
-/**
- * execute - function to execute commands
- * @command: input froom user
- * @envp: enviroment path
- * Return: -1 if failed and 0 if success
- */
-
-int execute(char *const command[], char **envp)
-{
-	pid_t id;
-	int status, i;
-	char **temp = pathfinder(command[0], (char **) command, envp);
-
-	if (temp != NULL)
-	{
-		id = fork();
-		if (id < 0)
-		{
-			perror("fork failed");
-			return (-1);
-		} else if (id == 0)
-		{
-			if (_getenv("PATH", envp) == NULL && access(command[0], F_OK) != 0)
-				printerror(command);
-			execve(temp[0], command, envp);
-			for (i = 0; command[i] != NULL; i++)
-				free(command[i]);
-			exit(EXIT_FAILURE);
-		}
-		wait(&status);
-		free(fullpath);
-	} else
-		printerror(command);
-	if (errno == -1)
-	{
-		free(*command);
-		exit(2);
+		dprintf(STDERR_FILENO, "%s: %s: not found\n", program_name, args[0]);
+		return;
 	}
-	return (0);
+	pid = fork();
+
+	if (pid == -1)
+	/* Search PATH for command */
+	path = getenv("PATH");
+	path_copy = strdup(path);
+	dir = strtok(path_copy, ":");
+
+	while (dir != NULL)
+	{
+		perror("fork failed");
+		exit(1);
+		snprintf(full_path, sizeof(full_path), "%s/%s", dir, args[0]);
+		if (access(full_path, X_OK) == 0)
+		{
+			args[0] = strdup(full_path);
+			found = 1;
+			break;
+		}
+		dir = strtok(NULL, ":");
+	}
+	free(path_copy);
+
+	/* If not found in PATH */
+	if (!found)
+	{
+		dprintf(STDERR_FILENO, "%s: %s: not found\n", program_name, args[0]);
+		return;
+	}
+
+	/* Execute */
+	pid = fork();
+	if (pid == 0)
+	{
+		    if (execve(args[0], args, environ) == -1)
+		if (execve(args[0], args, environ) == -1)
+		{
+			perror(program_name);
+			exit(1);
+		}
+	}
+
+	else
+	{
+		wait(NULL);
+	}
+	wait(NULL);
 }
