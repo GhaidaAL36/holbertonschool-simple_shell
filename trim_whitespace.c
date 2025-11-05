ead7aabcae1883a6d31d9311635b49e55c039532
lina_aziz <lenaexo0@gmail.com>
2025-11-06 00:52:50 +0300

    Update trim_whitespace.c

--- trim_whitespace.c
+++ trim_whitespace.c
@@ -1,26 +1,19 @@
 #include "simple_shell.h"
 
 /**
- * trim_whitespace - Removes leading and trailing spaces from a string
- * @str: The string to trim
- * Return: pointer to the trimmed string
+ * trim_whitespace - removes leading and trailing whitespace
+ * @str: input string
  */
-char *trim_whitespace(char *str)
+void trim_whitespace(char *str)
 {
 	char *end;
 
 	while (*str == ' ' || *str == '\t' || *str == '\n')
 		str++;
 
-	if (*str == 0)
-		return (str);
-
 	end = str + strlen(str) - 1;
-
 	while (end > str && (*end == ' ' || *end == '\t' || *end == '\n'))
 		end--;
 
 	*(end + 1) = '\0';
-
-	return str;
 }
