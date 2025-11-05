e55fe7b075016377c3473b69134ebe0caae3b430
lina_aziz <lenaexo0@gmail.com>
2025-11-06 01:24:28 +0300

    Update simple_shell.h

--- simple_shell.h
+++ simple_shell.h
@@ -1,19 +1,31 @@
-#ifndef SIMPLE_SHELL_H
-#define SIMPLE_SHELL_H
+#ifndef SIMPLE_SHELL
+#define SIMPLE_SHELL
 
-#include <stdio.h>
+/* LIBRARIES */
+
 #include <stdlib.h>
-#include <unistd.h>
+#include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdint.h>
 #include <sys/types.h>
-#include <sys/stat.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+
+
+/* GLOBAL VARIABLE */
 
 extern char **environ;
 
-char *read_line(void);
-char **split_line(char *line);
-void execute_command(char **args);
-void print_prompt(void);
+
+/* PROTOTYPES */
+
+int find_cmd_path(char *cmd, char *work_buffer);
+int file_exist(char *file);
+int execute_command(char *argv[]);
+char **fill_args(char *input_buffer);
+void print_env(void);
+int shell_error(void);
+char *_getenv(const char *name);
 
 #endif
