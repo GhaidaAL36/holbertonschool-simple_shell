63607bbf4d919c1be763ea39ceb77110e9d22818
lina_aziz <lenaexo0@gmail.com>
2025-11-06 02:50:21 +0300

    Update main.h

--- main.h
+++ main.h
@@ -3,20 +3,19 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
-#include <sys/stat.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 
 extern char **environ;
 
 char *read_line(void);
-char **get_argument(char *line);
-void free_args(char **args);
-void execute_command(char **args);
+char **tokenize(char *line);
 char *find_path(char *command);
-void print_env(void);
+void execute(char **args);
 int _strcmp(char *s1, char *s2);
+void free_args(char **args);
 
 #endif
