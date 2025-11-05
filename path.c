38c49b8af8560feba75fb311f28b0ca8b26556a6
lina_aziz <lenaexo0@gmail.com>
2025-11-06 02:50:41 +0300

    Update path.c

--- path.c
+++ path.c
@@ -1,34 +1,31 @@
 #include "main.h"
 
+/**
+ * find_path - find full path for a command using PATH
+ */
 char *find_path(char *command)
 {
-	char *path_env, *path_copy, *token, *full_path;
-	struct stat st;
+    char *path = getenv("PATH");
+    char *token, *full_path;
+    struct stat st;
 
-	path_env = getenv("PATH");
-	if (!path_env)
-		return (NULL);
+    if (!path)
+        return (NULL);
 
-	path_copy = strdup(path_env);
-	token = strtok(path_copy, ":");
-	while (token)
-	{
-		full_path = malloc(strlen(token) + strlen(command) + 2);
-		if (!full_path)
-		{
-			free(path_copy);
-			return (NULL);
-		}
-		sprintf(full_path, "%s/%s", token, command);
+    token = strtok(path, ":");
+    while (token)
+    {
+        full_path = malloc(strlen(token) + strlen(command) + 2);
+        if (!full_path)
+            return (NULL);
 
-		if (stat(full_path, &st) == 0)
-		{
-			free(path_copy);
-			return (full_path);
-		}
-		free(full_path);
-		token = strtok(NULL, ":");
-	}
-	free(path_copy);
-	return (NULL);
+        sprintf(full_path, "%s/%s", token, command);
+        if (stat(full_path, &st) == 0)
+            return (full_path);
+
+        free(full_path);
+        token = strtok(NULL, ":");
+    }
+
+    return (NULL);
 }
