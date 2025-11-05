738b14f3cff54059f922375fd3bc84e9b1f4ac23
lina_aziz <lenaexo0@gmail.com>
2025-11-06 01:43:45 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,20 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <sched.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
 
 extern char **environ;
 
-void exec(char **args, char *input);
-void tokenize(char *input, char *args[]);
-void handlePath(const char* path, int length);
-void printEnv(void);
-char *handle_path(char *input);
+void execute(char **argv);
+void print_env(void);
+char *find_in_path(const char *cmd);
+char *read_line(void);
+char **split_line(char *line);
 
-#endif /*MAIN_H*/
+#endif
