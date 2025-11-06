c8cc478ef2439a1180201e58f09fded7cb7873a1
lina_aziz <lenaexo0@gmail.com>
2025-11-06 03:44:05 +0300

    Update main.h

--- main.h
+++ main.h
@@ -7,12 +7,12 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/stat.h>
 
 extern char **environ;
 
 char **tokenize(char *line);
-char *find_path(char *cmd);
-void execute(char **argv);
+char *find_path(char *command);
+void execute(char **args);
+void free_tokens(char **tokens);
 
 #endif
