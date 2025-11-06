2c291e124bc0cd060d16cbb78d171336a1596d91
lina_aziz <lenaexo0@gmail.com>
2025-11-06 16:19:46 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,39 +1,38 @@
-#define _GNU_SOURCE
-
 #include "main.h"
-
-/**
- * main - main function for the shell
- * Return: 0 on success
- */
 
 int main(void)
 {
-	char *input = NULL;
-	char *args[64] = { NULL };
-	size_t inputSize = 0;
-	ssize_t inputRead;
+    char *input = NULL;
+    char *args[64] = { NULL };
+    size_t inputSize = 0;
+    ssize_t inputRead;
 
-	while (1)
-	{
-		if (isatty(STDIN_FILENO))
-		{
-			printf("$ ");
-			fflush(stdout);
-		}
+    while (1)
+    {
+        if (isatty(STDIN_FILENO))
+            write(STDOUT_FILENO, "$ ", 2);
 
-		inputRead = getline(&input, &inputSize, stdin);
-		if (inputRead == EOF)
-		{
-			free(input);
-			exit(0);
-		}
+        inputRead = getline(&input, &inputSize, stdin);
+        if (inputRead == EOF)
+        {
+            free(input);
+            return (0);
+        }
 
-		if (inputRead > 0 && input[inputRead - 1] == '\n')
-			input[inputRead - 1] = '\0';
+        if (inputRead > 0 && input[inputRead - 1] == '\n')
+            input[inputRead - 1] = '\0';
 
-		tokenize(input, args);
-	}
-	free(input);
-	return (0);
+        {
+            char *p = input;
+            while (*p == ' ' || *p == '\t')
+                p++;
+            if (*p == '\0')
+                continue;
+        }
+
+        tokenize(input, args);
+    }
+
+    free(input);
+    return (0);
 }
