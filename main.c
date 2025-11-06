7dd3d569acf099a3b9da0d09eaef517a3c23a235
lina_aziz <lenaexo0@gmail.com>
2025-11-06 05:21:54 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,40 +1,81 @@
-
-#define _GNU_SOURCE
-
 #include "main.h"
 
 /**
- * main - main function for the shell
- * Return: 0 on success
+ * print_env - prints all environment variables
+ * Return: 0
  */
+int print_env(void)
+{
+	int i = 0;
 
+	while (environ[i])
+	{
+		write(STDOUT_FILENO, environ[i], strlen(environ[i]));
+		write(STDOUT_FILENO, "\n", 1);
+		i++;
+	}
+	return (0);
+}
+
+/**
+ * parse - split command line and execute
+ * @command: user input
+ * @envp: environment pointer (unused)
+ */
+void parse(char command[], char **envp)
+{
+	char *args[64];
+	char *token;
+	int i = 0;
+
+	(void) envp;
+	token = strtok(command, " \t\n");
+	while (token)
+	{
+		args[i++] = token;
+		token = strtok(NULL, " \t\n");
+	}
+	args[i] = NULL;
+
+	if (args[0])
+	{
+		if (strcmp(args[0], "exit") == 0)
+			exit(0);
+		if (strcmp(args[0], "env") == 0)
+		{
+			print_env();
+			return;
+		}
+		execute(args, environ);
+	}
+}
+
+/**
+ * main - simple shell main loop
+ * Return: 0
+ */
 int main(void)
 {
-	char *input = NULL;
-	char *args[64] = { NULL };
-	size_t inputSize = 0;
-	ssize_t inputRead;
+	char *line = NULL;
+	size_t size = 0;
+	ssize_t nread;
 
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "($) ", 4);
+
+		nread = getline(&line, &size, stdin);
+		if (nread == -1)
 		{
-			printf("$ ");
-			fflush(stdout);
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			free(line);
+			break;
 		}
-
-		inputRead = getline(&input, &inputSize, stdin);
-		if (inputRead == EOF)
-		{
-			free(input);
-			exit(0);
-		}
-
-		if (inputRead > 0 && input[inputRead - 1] == '\n')
-			input[inputRead - 1] = '\0';
-
-		tokenize(input, args);
+		if (nread > 1)
+			parse(line, environ);
 	}
-	free(input);
+	free(line);
 	return (0);
 }
