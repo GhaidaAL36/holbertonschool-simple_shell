c3a01e473a2bb4a761b8beb3fac246549eb55e34
lina_aziz <lenaexo0@gmail.com>
2025-11-06 05:22:18 +0300

    Update exec.c

--- exec.c
+++ exec.c
@@ -1,3 +1,76 @@
+#include "main.h"
+
+/**
+ * _getenv - get environment variable value
+ * @name: variable name
+ * Return: pointer to value or NULL
+ */
+char *_getenv(const char *name)
+{
+	int i = 0;
+	size_t len = strlen(name);
+
+	while (environ[i])
+	{
+		if (strncmp(environ[i], name, len) == 0 && environ[i][len] == '=')
+			return (environ[i] + len + 1);
+		i++;
+	}
+	return (NULL);
+}
+
+/**
+ * pathfinder - locate executable in PATH
+ * @cmd: command name
+ * @command: argv array
+ * Return: updated argv or NULL
+ */
+char **pathfinder(char *cmd, char **command)
+{
+	char *path = _getenv("PATH");
+	char *dir, *dup, *full;
+	int len;
+
+	if (access(cmd, X_OK) == 0)
+	{
+		command[0] = cmd;
+		return (command);
+	}
+
+	if (!path)
+		return (NULL);
+
+	dup = strdup(path);
+	dir = strtok(dup, ":");
+	while (dir)
+	{
+		len = strlen(dir) + strlen(cmd) + 2;
+		full = malloc(len);
+		if (!full)
+		{
+			free(dup);
+			return (NULL);
+		}
+		sprintf(full, "%s/%s", dir, cmd);
+		if (access(full, X_OK) == 0)
+		{
+			command[0] = full;
+			free(dup);
+			return (command);
+		}
+		free(full);
+		dir = strtok(NULL, ":");
+	}
+	free(dup);
+	return (NULL);
+}
+
+/**
+ * execute - execute a given command
+ * @command: argv array
+ * @envp: environment (unused)
+ * Return: 0 on success
+ */
 int execute(char *const command[], char **envp)
 {
 	pid_t pid;
@@ -14,7 +87,6 @@ int execute(char *const command[], char **envp)
 		return (127);
 	}
 
-	/* إذا كان تم عمل malloc لمسار جديد */
 	if (temp[0] != command[0])
 		to_free = temp[0];
 
