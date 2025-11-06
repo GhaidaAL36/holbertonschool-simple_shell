a68cb344c2c3fb367b7c7569a7cb7802065cf55c
lina_aziz <lenaexo0@gmail.com>
2025-11-06 16:05:11 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,48 +1,39 @@
+#define _GNU_SOURCE
+
 #include "main.h"
 
 /**
- * main - The main function of the shell.
- * Return: EXIT_SUCCESS on success or return code > 0 if error.
+ * main - main function for the shell
+ * Return: 0 on success
  */
 
 int main(void)
 {
-	char *input_buffer = NULL, **myargv;
-	size_t size_allocated;
-	int char_read, ret = EXIT_SUCCESS;
+	char *input = NULL;
+	char *args[64] = { NULL };
+	size_t inputSize = 0;
+	ssize_t inputRead;
 
-	signal(SIGINT, SIG_IGN); /* ignore ctrl-C signal */
-	do {
+	while (1)
+	{
 		if (isatty(STDIN_FILENO))
-			printf("\033[0;39m#simple_shell(%d)$ ", getpid());
-
-		fflush(stdin);
-		char_read = getline(&input_buffer, &size_allocated, stdin);
-
-		if (char_read == 1)
-			continue;
-		if (char_read == EOF)
 		{
-			free(input_buffer);
-			if (isatty(STDIN_FILENO))
-				putchar('\n');
-			return (ret); /* exit shell with ctrl-D or | */
+			printf("$ ");
+			fflush(stdout);
 		}
 
-		input_buffer[char_read - 1] = 0; /* overwrite \n */
-		if (strncmp(input_buffer, "env", 3) == 0)
+		inputRead = getline(&input, &inputSize, stdin);
+		if (inputRead == EOF)
 		{
-			print_env();
-			continue;
+			free(input);
+			exit(0);
 		}
-		if (strncmp(input_buffer, "exit", 4) == 0)
-		{
-			free(input_buffer);
-			return (ret); /* ret= return code of last cmd before exit */
-		}
-		myargv = fill_args(input_buffer);
-		if (myargv[0] != NULL)
-			ret = execute_command(myargv);
-		free(myargv);
-	} while (1);
+
+		if (inputRead > 0 && input[inputRead - 1] == '\n')
+			input[inputRead - 1] = '\0';
+
+		tokenize(input, args);
+	}
+	free(input);
+	return (0);
 }
