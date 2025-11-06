cb9c06d16b126d93fbbbf7b3ebab1a890c5245a8
lina_aziz <lenaexo0@gmail.com>
2025-11-06 03:29:23 +0300

    Update main.h

--- main.h
+++ main.h
@@ -6,14 +6,13 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
-#include <sys/stat.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 
 extern char **environ;
 
 char **tokenize(char *line);
-char *get_env_path(void);
-char *find_command(char *cmd);
+char *find_path(char *cmd);
 void execute(char **argv);
 
 #endif
