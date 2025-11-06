2dfb52cb76429db0524318009db1c3b2bb77eb1a
lina_aziz <lenaexo0@gmail.com>
2025-11-06 03:03:28 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,50 +1,36 @@
 #include "main.h"
 
-/**
- * main - Simple shell that handles PATH
- */
 int main(void)
 {
     char *line = NULL;
-    char **args = NULL;
-    ssize_t read;
-    size_t len = 0;
+    size_t cap = 0;
+    ssize_t n;
 
     while (1)
     {
-        if (isatty(STDIN_FILENO))
+        if (is_interactive())
             write(STDOUT_FILENO, "$ ", 2);
 
-        read = getline(&line, &len, stdin);
-        if (read == -1)
-        {
-            free(line);
-            if (isatty(STDIN_FILENO))
-                write(STDOUT_FILENO, "\n", 1);
+        n = getline(&line, &cap, stdin);
+        if (n == -1)
             break;
-        }
 
-        if (line[0] == '\n')
+        trim_ends(line);
+        if (!*line || is_empty(line))
             continue;
 
-        line[read - 1] = '\0';
-        args = tokenize(line);
-        if (!args || !args[0])
+        char **argv = tokenize(line);
+        if (argv)
         {
-            free_args(args);
-            continue;
-        }
+            exec_command(argv);
 
-        if (_strcmp(args[0], "exit") == 0)
-        {
-            free_args(args);
-            break;
+            /* free argv */
+            for (size_t i = 0; argv[i]; i++)
+                free(argv[i]);
+            free(argv);
         }
-
-        execute(args);
-        free_args(args);
     }
 
     free(line);
-    return (0);
+    return 0;
 }
