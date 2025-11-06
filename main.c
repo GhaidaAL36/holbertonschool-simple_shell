5fe65d24f0f7945a81075c7b6600bad141fc8221
lina_aziz <lenaexo0@gmail.com>
2025-11-06 03:30:57 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,30 +3,23 @@
 int main(void)
 {
     char *line = NULL;
-    size_t cap = 0;
-    ssize_t n;
+    size_t size = 0;
+    ssize_t nread;
     char **argv;
-    size_t i;
 
     while (1)
     {
-        if (is_interactive())
+        if (isatty(STDIN_FILENO))
             write(STDOUT_FILENO, "$ ", 2);
 
-        n = getline(&line, &cap, stdin);
-        if (n == -1)
+        nread = getline(&line, &size, stdin);
+        if (nread == -1)
             break;
 
-        trim_ends(line);
-        if (!*line || is_empty(line))
-            continue;
-
-        argv = tokenize(line);
-        if (argv)
+        if (line[0] != '\n')
         {
-            exec_command(argv);
-            for (i = 0; argv[i]; i++)
-                free(argv[i]);
+            argv = tokenize(line);
+            execute(argv);
             free(argv);
         }
     }
