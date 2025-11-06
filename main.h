240f8579d36ef157b1593495f4475fda766bb49c
lina_aziz <lenaexo0@gmail.com>
2025-11-06 03:27:02 +0300

    Update main.h

--- main.h
+++ main.h
@@ -8,24 +8,12 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
-#include <fcntl.h>
-#include <errno.h>
 
 extern char **environ;
 
-/* utils */
-char *_getenv_val(const char *name);
-int is_interactive(void);
-void trim_ends(char *s);
-int is_empty(const char *s);
+char **tokenize(char *line);
+char *get_env_path(void);
+char *find_command(char *cmd);
+void execute(char **argv);
 
-/* tokenizer */
-char **tokenize(const char *line);
-
-/* path */
-char *find_in_path(const char *cmd);
-
-/* exec */
-void exec_command(char **argv);
-
-#endif /* MAIN_H */
+#endif
