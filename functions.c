c6b4ee14658fd0450d7fef7cb9c9212c0df03de4
lina_aziz <lenaexo0@gmail.com>
2025-11-06 16:20:25 +0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,128 +1,94 @@
 #include "main.h"
 
-
-/**
- * _err - checks and handles errors
- * @args: arguments to check
- * Return: void
- */
-
-void _err(char *args[])
+static void print_not_found(char *cmd)
 {
-	fprintf(stderr, "%s: command not found\n", args[0]);
-	perror("");
-	free(args[0]);
-	exit(98);
+    write(STDERR_FILENO, cmd, strlen(cmd));
+    write(STDERR_FILENO, ": not found\n", 12);
 }
-
-
-/**
- * exec - executes the input received
- * @args: arguments
- * @input: input
- * Return: void
- */
-
-void exec(char **args, char *input)
-{
-
-	int status, statusExit;
-	pid_t childPid = 0;
-
-	if (access(args[0], X_OK) != 0)
-		_err(args);
-
-	childPid = fork();
-
-	if (childPid == -1)
-	{
-		perror("fork\n");
-		free(input);
-		exit(0);
-	}
-	else if (childPid == 0)
-	{
-		execve(args[0], args, environ);
-		free(args[0]);
-		exit(0);
-	}
-	else
-	{
-		wait(&status);
-		if (WIFEXITED(status))
-		{
-			statusExit = WEXITSTATUS(status);
-			if (statusExit != 0)
-			{
-				free(args[0]);
-				free(input);
-				exit(0);
-			}
-		}
-	}
-}
-
-/**
- * printEnv - print the environment variables
- * Return: void
- */
 
 void printEnv(void)
 {
-	char **env;
+    char **env;
 
-	for (env = environ; *env != NULL; env++)
-	{
-		printf("%s\n", *env);
-	}
+    for (env = environ; *env != NULL; env++)
+    {
+        write(STDOUT_FILENO, *env, strlen(*env));
+        write(STDOUT_FILENO, "\n", 1);
+    }
 }
 
-/**
- * tokenize - function that splits a string into multiple ones
- * @input: users input
- * @args: arguments
- * Return: void
- */
+void exec(char **args, char *input)
+{
+    pid_t childPid;
+    int status;
+
+    (void)input;
+
+    if (access(args[0], X_OK) != 0)
+    {
+        print_not_found(args[0]);
+        return;
+    }
+
+    childPid = fork();
+    if (childPid == -1)
+    {
+        perror("fork");
+        return;
+    }
+    else if (childPid == 0)
+    {
+        execve(args[0], args, environ);
+        perror("execve");
+        _exit(127);
+    }
+    else
+    {
+        waitpid(childPid, &status, 0);
+    }
+}
 
 void tokenize(char *input, char *args[])
 {
-	char *token;
-	unsigned int i = 0;
+    char *token;
+    size_t i = 0;
+    char *full = NULL;
 
-	token = strtok(input, " ");
-	while (token != NULL)
-	{
-		args[i] = token;
-		i++;
-		token = strtok(NULL, " ");
-	}
-	args[i] = NULL;
+    for (i = 0; i < 64; i++)
+        args[i] = NULL;
+    i = 0;
 
-	if (args[0] == NULL)
-		exit(0);
+    token = strtok(input, " \t");
+    while (token)
+    {
+        args[i++] = token;
+        token = strtok(NULL, " \t");
+    }
+    args[i] = NULL;
 
-	if (strcmp(input, "env") == 0)
-	{
-		printEnv();
-		return;
-	}
+    if (args[0] == NULL)
+        return;
 
-	if (strcmp(input, "exit") == 0 && args[1] == NULL)
-	{
-		free(args[0]);
-		exit(0);
-	}
+    if (strcmp(args[0], "env") == 0)
+    {
+        printEnv();
+        return;
+    }
 
-	token = strdup(args[0]);
-	args[0] = handle_path(args[0]);
-	if (args[0] != NULL)
-	{
-		free(token);
-		exec(args, input);
-		free(args[0]);
-		return;
-	}
-	free(token);
-	fprintf(stderr, "%s: command not found\n", args[0]);
-	exit(127);
+    if (strcmp(args[0], "exit") == 0 && args[1] == NULL)
+        exit(0);
+
+    full = handle_path(args[0]);
+    if (full)
+    {
+        char *old0 = args[0];
+        args[0] = full;
+        exec(args, input);
+        free(args[0]);
+        args[0] = old0;
+    }
+    else
+    {
+        print_not_found(args[0]);
+    }
 }
