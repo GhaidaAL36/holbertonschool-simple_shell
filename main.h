d2563ce08c89ebe6aeb8642a94f033c2134ef5de
lina_aziz <lenaexo0@gmail.com>
2025-11-06 04:52:55 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,19 +1,24 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*Librairies */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sched.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
 extern char **environ;
 
-int execute(char *const command[], char **envp);
-char *_getenv(const char *name);
-char **pathfinder(char *cmd, char **command);
-void parse(char command[], char **envp);
-int print_env(void);
+/* Prototypes */
 
-#endif /* MAIN_H */
+void exec(char **args, char *input);
+void tokenize(char *input, char *args[]);
+void handlePath(const char* path, int length);
+void printEnv(void);
+char *handle_path(char *input);
+
+#endif
