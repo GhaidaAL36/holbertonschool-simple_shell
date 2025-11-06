c054add53f3251f738ab28b2d9668d66e531f6ad
lina_aziz <lenaexo0@gmail.com>
2025-11-06 15:06:38 +0300

    Update tokenize.c

--- tokenize.c
+++ tokenize.c
@@ -1,51 +1,43 @@
 #include "main.h"
 
 /**
- * tokenize - function that splits a string into multiple ones
- * @input: users input
- * @args: arguments
- * Return: void
+ * tokenize - splits a string into tokens and executes
+ * @input: user input
+ * @args: argument array
  */
-
 void tokenize(char *input, char *args[])
 {
 	char *token;
 	unsigned int i = 0;
 
-	token = strtok(input, " ");
+	token = strtok(input, " \t");
 	while (token != NULL)
 	{
-		args[i] = token;
-		i++;
-		token = strtok(NULL, " ");
+		args[i++] = token;
+		token = strtok(NULL, " \t");
 	}
 	args[i] = NULL;
 
 	if (args[0] == NULL)
-		exit(0);
+		return;
 
-	if (strcmp(input, "env") == 0)
+	if (strcmp(args[0], "env") == 0)
 	{
 		printEnv();
 		return;
 	}
-
-	if (strcmp(input, "exit") == 0 && args[1] == NULL)
+	if (strcmp(args[0], "exit") == 0 && args[1] == NULL)
 	{
-		free(args[0]);
+		free(input);
 		exit(0);
 	}
 
-	token = strdup(args[0]);
 	args[0] = handle_path(args[0]);
 	if (args[0] != NULL)
 	{
-		free(token);
 		exec(args, input);
 		free(args[0]);
-		return;
 	}
-	fprintf(stderr, "./hsh: 1: %s: not found\n", token);
-	free(token);
-	exit(127);
+	else
+		fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
 }
