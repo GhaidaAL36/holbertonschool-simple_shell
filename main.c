21678c10cf34e2cc348680abcc8ad5722c28e749
lina_aziz <lenaexo0@gmail.com>
2025-11-06 16:38:10 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,132 +1,56 @@
-#include "simpleshell.h"
+#define _GNU_SOURCE
 
-/*char **env, *command;*/
+#include "main.h"
 
 /**
- * print_env - prints the environment
- * Return: zero
+ * spacesCheck - check if str contain only space
+ * @str: string to check
+ * Return: 0 on success or 1 on failure
  */
 
-int print_env(void)
+int spacesCheck(const char *str)
 {
-	int i = 0;
-
-	if (env == NULL)
-		return (-1);
-	while (env[i])
+	while (*str)
 	{
-		printf("%s\n", env[i++]);
+		if (*str != ' ')
+			return (0);
+		str++;
 	}
-	return (0);
+	return (1);
 }
 
 /**
- * parse - tokenizes
- * @command: command from the user
- * @envp: enviroment path
+ * main - main function for the shell
+ * Return: 0 on success
  */
 
-void parse(char command[], char **envp)
+int main(void)
 {
-	char *arguments[11];
-	char *token = strtok(command, " ");
-	int arg_count = 0;
+	char *input = NULL;
+	char *args[64] = { NULL };
+	size_t inputSize = 0;
+	ssize_t inputRead;
 
-
-	while (token != NULL && arg_count < 10)
-	{
-		arguments[arg_count++] = token;
-		token = strtok(NULL, " ");
-	}
-	if (arg_count > 11)
-	{
-		perror("Error: Too many arguments");
-		return;
-	}
-	arguments[arg_count] = NULL;
-	if (arg_count > 0)
-	{
-		if (envp != NULL)
-			execute(arguments, envp);
-		else
-		{
-			fprintf(stderr, "./hsh: 1: %s: not found\n", arguments[0]);
-			exit(127);
-		}
-	}
-}
-
-/**
- * input - function to keep prompting user until exit
- * @command: argument to process
- * @size: size of argument
- */
-
-void input(char **command, size_t *size)
-{
-	size_t read_bytes;
-
-	read_bytes = getline(command, size, stdin);
-	if ((int) read_bytes == EOF)
-	{
-		if (isatty(STDIN_FILENO) != 0)
-			printf("\n");
-		if (*command != NULL)
-			free(*command);
-		exit(EXIT_SUCCESS);
-	}
-	if ((*command)[read_bytes - 1] == '\n')
-		(*command)[read_bytes - 1] = '\0';
-}
-
-/**
- * main - Shell, interactive or non interactive
- * @argc: argument count
- * @argv: argument vector
- * @envp: environmental variable
- * Return: 1 if command fails
- */
-
-int main(int argc, char *argv[], char **envp)
-{
-	size_t size;
-
-	env = envp;
-	command = NULL;
-	(void) argv;
-	if (argc > 1)
-	{
-		printf("./shell: command does not exist\n");
-		exit(EXIT_FAILURE);
-	}
 	while (1)
 	{
-		size = 0;
 		if (isatty(STDIN_FILENO))
-			printf("($) ");
-		input(&command, &size);
-		if (strcmp(command, "exit") == 0)
 		{
-			free(command);
-			exit(EXIT_SUCCESS);
+			printf("$ ");
+			fflush(stdout);
 		}
-		if (strcmp(command, "env") == 0)
+
+		inputRead = getline(&input, &inputSize, stdin);
+		if (inputRead == EOF)
 		{
-			print_env();
-			free(command);
-			exit(EXIT_SUCCESS);
+			free(input);
+			exit(0);
 		}
-		if (_getenv("PATH", envp) == NULL)
-		{
-			if (command[0] != '/')
-			{
-				fprintf(stderr, "./hsh: 1: %s: not found\n", command);
-				free(command);
-				exit(127);
-			}
-		}
-		parse(command, envp);
-		free(command);
+
+		if (inputRead > 0 && input[inputRead - 1] == '\n')
+			input[inputRead - 1] = '\0';
+		if (spacesCheck(input) != 1)
+			tokenize(input, args);
 	}
+	free(input);
 	return (0);
 }
