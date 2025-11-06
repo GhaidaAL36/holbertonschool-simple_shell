0cf29841fb7e19ebbe6ccaed11995b1a625bf10c
lina_aziz <lenaexo0@gmail.com>
2025-11-06 03:06:15 +0300

    Update tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -1,45 +1,49 @@
 #include "main.h"
 
-/* split by spaces/tabs; returns NULL-terminated argv; malloc'd */
+/* Split input line by space/tab */
 char **tokenize(const char *line)
 {
     char *copy, *tok, **argv = NULL;
-    size_t cap = 0, len = 0;
+    size_t cap = 0, len = 0, i;
+    char **tmp;
 
-    if (!line) return NULL;
+    if (!line)
+        return (NULL);
 
     copy = strdup(line);
-    if (!copy) return NULL;
+    if (!copy)
+        return (NULL);
 
     tok = strtok(copy, " \t");
     while (tok)
     {
-        if (len + 2 > cap) /* +1 for tok, +1 for NULL */
+        if (len + 2 > cap)
         {
             size_t newcap = cap ? cap * 2 : 8;
-            char **tmp = realloc(argv, newcap * sizeof(char *));
+            tmp = realloc(argv, newcap * sizeof(char *));
             if (!tmp)
             {
-                size_t i;
-                for (i = 0; i < len; i++) free(argv[i]);
+                for (i = 0; i < len; i++)
+                    free(argv[i]);
                 free(argv);
                 free(copy);
-                return NULL;
+                return (NULL);
             }
             argv = tmp;
             cap = newcap;
         }
-        argv[len++] = strdup(tok);
+        argv[len] = strdup(tok);
+        len++;
         tok = strtok(NULL, " \t");
     }
 
-    if (!argv) /* no tokens */
+    if (!argv)
     {
         free(copy);
-        return NULL;
+        return (NULL);
     }
 
     argv[len] = NULL;
     free(copy);
-    return argv;
+    return (argv);
 }
