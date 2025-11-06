53af72b251b5ebb32074192bccecd01f66e6277f
lina_aziz <lenaexo0@gmail.com>
2025-11-06 03:44:22 +0300

    Update tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -1,21 +1,40 @@
 #include "main.h"
 
+/**
+ * tokenize - split line into tokens
+ * @line: input line
+ * Return: array of tokens
+ */
 char **tokenize(char *line)
 {
-    char **argv;
+    char **tokens;
     char *token;
     int i = 0;
 
-    argv = malloc(sizeof(char *) * 64);
-    if (!argv)
+    tokens = malloc(sizeof(char *) * 64);
+    if (!tokens)
         return (NULL);
 
     token = strtok(line, " \t\n");
     while (token)
     {
-        argv[i++] = token;
+        tokens[i] = strdup(token);
+        i++;
         token = strtok(NULL, " \t\n");
     }
-    argv[i] = NULL;
-    return (argv);
+    tokens[i] = NULL;
+    return (tokens);
+}
+
+/**
+ * free_tokens - free tokenized array
+ * @tokens: array to free
+ */
+void free_tokens(char **tokens)
+{
+    int i;
+
+    for (i = 0; tokens[i]; i++)
+        free(tokens[i]);
+    free(tokens);
 }
