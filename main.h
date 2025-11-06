6ff3db94068ba543089197e5c8d47febb87ff4a4
lina_aziz <lenaexo0@gmail.com>
2025-11-06 03:05:35 +0300

    Update main.h

--- main.h
+++ main.h
@@ -15,9 +15,9 @@ extern char **environ;
 
 /* utils */
 char *_getenv_val(const char *name);
-int   is_interactive(void);
-void  trim_ends(char *s);
-int   is_empty(const char *s);
+int is_interactive(void);
+void trim_ends(char *s);
+int is_empty(const char *s);
 
 /* tokenizer */
 char **tokenize(const char *line);
@@ -26,6 +26,6 @@ char **tokenize(const char *line);
 char *find_in_path(const char *cmd);
 
 /* exec */
-void  exec_command(char **argv);
+void exec_command(char **argv);
 
 #endif /* MAIN_H */
