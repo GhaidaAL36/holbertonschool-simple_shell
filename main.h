56f46033e98f00f8060d7a42279c58b09cc1fb10
lina_aziz <lenaexo0@gmail.com>
2025-11-09 09:28:20 +0300

    Update main.h

--- main.h
+++ main.h
@@ -5,16 +5,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sched.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
 extern char **environ;
 
+/* Function prototypes */
 void exec(char **args, char *input);
 void tokenize(char *input, char *args[]);
-void handlePath(const char* path, int length);
 void printEnv(void);
 char *handle_path(char *input);
 
-#endif /*MAIN_H*/
+#endif /* MAIN_H */
