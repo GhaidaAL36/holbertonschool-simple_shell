6390461ebebda9b6295691e0090fc6ae57d57e3b
lina_aziz <lenaexo0@gmail.com>
2025-11-09 09:27:23 +0300

    Update handlePath.c

--- handlePath.c
+++ handlePath.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * handle_path - Finds the path of the command to execute
- * @input: User input
- * Return: The full path of the command if found, NULL otherwise
+ * handle_path - Find the full path of a command
+ * @input: User input command
+ *
+ * Return: Full path if found, otherwise NULL
  */
-
 char *handle_path(char *input)
 {
 	int i = 0;
@@ -17,20 +17,26 @@ char *handle_path(char *input)
 	while (environ[i] != NULL)
 	{
 		cache = strdup(environ[i]);
+		if (cache == NULL)
+			return (NULL);
+
 		token = strtok(cache, "=");
-		if (strcmp(token, "PATH") == 0)
+		if (token != NULL && strcmp(token, "PATH") == 0)
 		{
 			token = strtok(NULL, "=");
 			token = strtok(token, ":");
+
 			while (token != NULL)
 			{
 				result = malloc(strlen(token) + strlen(input) + 2);
 				if (result == NULL)
 				{
-					perror("Malloc is NULL");
+					free(cache);
 					return (NULL);
 				}
+
 				sprintf(result, "%s/%s", token, input);
+
 				if (access(result, X_OK) == 0)
 				{
 					free(cache);
@@ -41,6 +47,7 @@ char *handle_path(char *input)
 				token = strtok(NULL, ":");
 			}
 		}
+
 		free(cache);
 		i++;
 	}
