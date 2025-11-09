6067f4a4f70e6e0b35f15a5a6a16838122880324
lina_aziz <lenaexo0@gmail.com>
2025-11-09 09:33:12 +0300

    Update handlePath.c

--- handlePath.c
+++ handlePath.c
@@ -1,50 +1,30 @@
 #include "main.h"
 
 /**
- * handle_path - Find the full path of a command
- * @input: User input command
+ * get_path_env - Get PATH variable from environment
  *
- * Return: Full path if found, otherwise NULL
+ * Return: Copy of PATH value or NULL
  */
-char *handle_path(char *input)
+char *get_path_env(void)
 {
 	int i = 0;
-	char *cache, *token, *result;
-
-	if (strchr(input, '/') != NULL)
-		return (strdup(input));
+	char *cache, *token;
 
 	while (environ[i] != NULL)
 	{
 		cache = strdup(environ[i]);
-		if (cache == NULL)
+		if (!cache)
 			return (NULL);
 
 		token = strtok(cache, "=");
-		if (token != NULL && strcmp(token, "PATH") == 0)
+		if (token && strcmp(token, "PATH") == 0)
 		{
 			token = strtok(NULL, "=");
-			token = strtok(token, ":");
-
-			while (token != NULL)
+			if (token)
 			{
-				result = malloc(strlen(token) + strlen(input) + 2);
-				if (result == NULL)
-				{
-					free(cache);
-					return (NULL);
-				}
-
-				sprintf(result, "%s/%s", token, input);
-
-				if (access(result, X_OK) == 0)
-				{
-					free(cache);
-					return (result);
-				}
-
-				free(result);
-				token = strtok(NULL, ":");
+				token = strdup(token);
+				free(cache);
+				return (token);
 			}
 		}
 
@@ -52,6 +32,65 @@ char *handle_path(char *input)
 		i++;
 	}
 
-	free(input);
+	return (NULL);
+}
+
+/**
+ * build_full_path - Create full path from directory + command
+ * @dir: Directory
+ * @cmd: Command
+ *
+ * Return: Allocated full path or NULL
+ */
+char *build_full_path(char *dir, char *cmd)
+{
+	char *result;
+
+	result = malloc(strlen(dir) + strlen(cmd) + 2);
+	if (result == NULL)
+		return (NULL);
+
+	sprintf(result, "%s/%s", dir, cmd);
+	return (result);
+}
+
+/**
+ * handle_path - Resolve full path of a command
+ * @input: Command
+ *
+ * Return: Full path or NULL
+ */
+char *handle_path(char *input)
+{
+	char *path_env, *token, *full;
+
+	if (strchr(input, '/'))
+		return (strdup(input));
+
+	path_env = get_path_env();
+	if (!path_env)
+		return (NULL);
+
+	token = strtok(path_env, ":");
+	while (token)
+	{
+		full = build_full_path(token, input);
+		if (!full)
+		{
+			free(path_env);
+			return (NULL);
+		}
+
+		if (access(full, X_OK) == 0)
+		{
+			free(path_env);
+			return (full);
+		}
+
+		free(full);
+		token = strtok(NULL, ":");
+	}
+
+	free(path_env);
 	return (NULL);
 }
