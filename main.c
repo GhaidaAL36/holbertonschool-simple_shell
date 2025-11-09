43d624e082c69da7b6ec71c081dadf8ada14a7e4
GhaidaAl36 <ghaida3638@gmail.com>
2025-11-09 12:41:07 +0000

    handel ls

--- main.c
+++ main.c
@@ -1,34 +1,60 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 /**
-	* main - simple UNIX command line interpreter
-	* @argc: argument count
-	* @argv: argument vector
-	* @envp: environment variables
-	* Return: 0 on success
-	*/
+ * is_executable - check if a file is executable
+ * @path: path to file
+ * Return: 1 if executable, 0 otherwise
+ */
 int is_executable(char *path)
 {
     return (access(path, X_OK) == 0);
 }
 
+/**
+ * strdup_c90 - duplicate a string (C90 safe)
+ * @s: string to duplicate
+ * Return: pointer to duplicated string
+ */
+char *strdup_c90(const char *s)
+{
+    char *dup = malloc(strlen(s) + 1);
+    if (dup)
+        strcpy(dup, s);
+    return dup;
+}
+
+/**
+ * find_command - find full path of command using PATH
+ * @cmd: command
+ * Return: malloced full path, or NULL if not found
+ */
 char *find_command(char *cmd)
 {
-    char *path_env = getenv("PATH");
-    char *path_dup, *dir;
-    char *full_path = NULL;
+    char *path_env;
+    char *path_dup;
+    char *dir;
+    char *full_path;
 
-    if (!path_env || !cmd)
+    if (!cmd)
         return NULL;
 
-    if (strchr(cmd, '/')) /* command has a '/' -> absolute or relative path */
+    if (strchr(cmd, '/')) /* absolute or relative path */
     {
         if (is_executable(cmd))
-            return strdup(cmd);
+            return strdup_c90(cmd);
         return NULL;
     }
 
-    path_dup = strdup(path_env);
+    path_env = getenv("PATH");
+    if (!path_env)
+        return NULL;
+
+    path_dup = strdup_c90(path_env);
     if (!path_dup)
         return NULL;
 
@@ -51,22 +77,32 @@ char *find_command(char *cmd)
         full_path = NULL;
         dir = strtok(NULL, ":");
     }
+
     free(path_dup);
     return NULL;
 }
 
-
+/**
+ * main - simple shell
+ * @argc: argument count
+ * @argv: argument vector
+ * @envp: environment variables
+ * Return: 0
+ */
 int main(int argc, char **argv, char **envp)
 {
-    char *line = NULL;
-    size_t len = 0;
+    char *line;
+    size_t len;
     ssize_t nread;
     pid_t pid;
     int status;
-    char *command; /* declaration at top for C90 */
+    char *command;
     char *path_cmd;
 
     (void)argc;
+
+    line = NULL;
+    len = 0;
 
     while (1)
     {
@@ -85,21 +121,23 @@ int main(int argc, char **argv, char **envp)
         if (!_isspace(command))
         {
             char *args[100];
-            int i = 0;
-            char *token = strtok(command, " \t\n");
-            while (token != NULL && i < 99)
+            int i;
+            char *token;
+
+            i = 0;
+            token = strtok(command, " \t\n");
+            while (token && i < 99)
             {
                 args[i++] = token;
                 token = strtok(NULL, " \t\n");
             }
             args[i] = NULL;
 
-            /* Find command in PATH */
             path_cmd = find_command(args[0]);
             if (!path_cmd)
             {
-                fprintf(stderr, "%s: command not found\n", argv[0]);
-                continue; /* do NOT fork if command does not exist */
+                fprintf(stderr, "%s: command not found\n", args[0]);
+                continue;
             }
 
             pid = fork();
@@ -112,7 +150,7 @@ int main(int argc, char **argv, char **envp)
             else if (pid == 0)
             {
                 execve(path_cmd, args, envp);
-                perror(argv[0]);
+                perror(args[0]);
                 exit(EXIT_FAILURE);
             }
             else
