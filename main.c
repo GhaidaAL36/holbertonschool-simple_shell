d0f2ec5f1b761bc2b77a0c5d23420cb3d3af3960
lina_aziz <lenaexo0@gmail.com>
2025-11-12 15:56:31 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * main - entry point for simple shell
- * @argc: argument count
- * @argv: argument vector
- * @envp: environment variables
- *
- * Return: last command exit status
- */
+	* main - entry point for simple shell
+	* @argc: argument count
+	* @argv: argument vector
+	* @envp: environment variables
+	*
+	* Return: last command exit status
+	*/
 int main(int argc, char **argv, char **envp)
 {
 	char *line = NULL;
@@ -19,38 +19,30 @@ int main(int argc, char **argv, char **envp)
 
 	(void)argc;
 	(void)argv;
-
-	signal(SIGINT, handle_sigint);
+	
+signal(SIGINT, handle_sigint);
 
 	while (1)
 	{
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "$ ", 2);
+	if (isatty(STDIN_FILENO))
+	write(STDOUT_FILENO, ":) ", 3);
 
-		nread = _getline(&line, &len);
-		if (nread == -1)
-		{
-			if (isatty(STDIN_FILENO))
-				write(STDOUT_FILENO, "\n", 1);
-			break;
-		}
+	nread = getline(&line, &len, stdin);
+	if (nread == -1)
+	{
+	if (isatty(STDIN_FILENO))
+	write(STDOUT_FILENO, "\n", 1);
+	break;
+	}
 
-		if (_strstr(line, "&&") || _strstr(line, "||"))
-		{
-			handle_logical_operators(line, envp);
-			continue;
-		}
-
-		command = trim(line);
-		if (!_isspace(command))
-		{
-			parse_command(command, args);
-
-			if (handle_builtins(args, line, envp, &status))
-				continue;
-
-			status = execute_command(args, envp);
-		}
+	command = trim(line);
+	if (!_isspace(command))
+	{
+	parse_command(command, args);
+	if (handle_builtins(args, line, envp, &status))
+	continue;
+	status = execute_command(args, envp);
+	}
 	}
 
 	free(line);
